3504c3f3dc6eeef29ae990c1f6a06ea9099aa1df
nico <nicolas.p.suarez@gmail.com>
2018-11-02 19:18:07 -0300

    Filter deleted entities

--- views/entityview.py
+++ views/entityview.py
@@ -3,7 +3,9 @@ from flask import flash
 from flask_admin import expose
 from flask_admin.babel import gettext
 from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import BaseQuery
 from models.entity import Entity
+from sqlalchemy import func, or_
 
 
 class EntityView(ModelView):
@@ -16,6 +18,20 @@ class EntityView(ModelView):
 
     # Remove fields from the create and edit forms.
     form_excluded_columns = ['deleted']
+
+    def get_query(self) -> BaseQuery:
+        """
+        Override. Filter deleted entities.
+        """
+        return super(EntityView, self).get_query().\
+            filter(or_(Entity.deleted == None, Entity.deleted == False))
+
+    def get_count_query(self) -> BaseQuery:
+        """
+        Override. Filter deleted entities.
+        """
+        return super(EntityView, self).get_count_query().\
+            filter(or_(Entity.deleted == None, Entity.deleted == False))
 
     def delete_model(self, model: Entity) -> bool:
         """
